50d64c0df0c872f1ff657449b24a16fc992badfd
Sam Sweet <samwsweet@gmail.com>
2025-09-16 21:46:50 -0400

    add ethermint type for keplr compatability.  update add-to-keplr to add evm and cosmos

--- crypto/codec/codec.go
+++ crypto/codec/codec.go
@@ -5,10 +5,17 @@ import (
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/gogoproto/proto"
 )
 
 // RegisterInterfaces register the Cosmos EVM key concrete types.
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ethsecp256k1.PubKey{})
 	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethsecp256k1.PrivKey{})
+}
+
+func init() {
+	// Register the same concrete types with Ethermint URLs for Keplr compatibility
+	proto.RegisterType((*ethsecp256k1.PubKey)(nil), "ethermint.crypto.v1.ethsecp256k1.PubKey")
+	proto.RegisterType((*ethsecp256k1.PrivKey)(nil), "ethermint.crypto.v1.ethsecp256k1.PrivKey")
 }
