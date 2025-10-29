91256c5e91ecb02358a14995b5556d23270ef865
Jason Lancaster <jason@shardeum.org>
2025-10-29 01:49:35 -0400

    Specify minimum gas price

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -27,7 +27,7 @@ esac
 
 case $NODE_TYPE in
   API)
-    OPTIONS='--rpc.laddr tcp://0.0.0.0:26657 --json-rpc.enable --json-rpc.address 0.0.0.0:8545 --api.enable --api.address tcp://0.0.0.0:1317 --json-rpc.ws-address 0.0.0.0:8546 --pruning nothing'
+    OPTIONS='--rpc.laddr tcp://0.0.0.0:26657 --json-rpc.enable --json-rpc.address 0.0.0.0:8545 --api.enable --api.address tcp://0.0.0.0:1317 --json-rpc.ws-address 0.0.0.0:8546 --pruning nothing --minimum-gas-prices=2048130280389041ashm'
     ;;
   VALIDATOR)
     OPTIONS='--p2p.laddr tcp://0.0.0.0:27656'
