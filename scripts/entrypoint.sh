1390746e275354322d752c22f13fd9dc66c8736c
Aniket Divekar <aniket.divekar@gmail.com>
2025-10-30 16:59:11 +0530

    fix: add evm.evm-chain-id

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -18,26 +18,29 @@ fi
 
 case $SHARDEUM_NETWORK in
   testnet)
-    CHAIN_ID=shardeum-testnet
+    CHAIN_ID=shardeum-8119-2
+    EVM_CHAIN_ID=8119
     cp /app/config/testnet-genesis.json /app/config/genesis.json
     ;;
   mainnet)
     CHAIN_ID=shardeum_8118-1
+    EVM_CHAIN_ID=8118
     cp /app/config/mainnet-genesis.json /app/config/genesis.json
     ;;
 esac
 
 case $NODE_TYPE in
   API)
-    OPTIONS='--rpc.laddr tcp://0.0.0.0:26657 \
+    OPTIONS="--rpc.laddr tcp://0.0.0.0:26657 \
              --api.enable \
              --api.address tcp://0.0.0.0:1317 \
              --json-rpc.enable \
              --json-rpc.address 0.0.0.0:8545 \
              --json-rpc.ws-address 0.0.0.0:8546 \
              --json-rpc.api eth,txpool,personal,net,debug,web3 \
+             --evm.evm-chain-id $EVM_CHAIN_ID \
              --minimum-gas-prices=2048130280389041ashm \
-             --pruning nothing'
+             --pruning nothing"
     ;;
   VALIDATOR)
     OPTIONS='--p2p.laddr tcp://0.0.0.0:27656'
